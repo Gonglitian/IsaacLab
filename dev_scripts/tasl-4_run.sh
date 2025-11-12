16cdbf274b6da17e023c738f3100a0ed253d1b99
Gonglitian <gongc9@qq.com>
2025-11-12 02:45:42 -0800

    Update num_mini_batches parameter in tasl-4_run.sh from 80000 to 8 for improved training efficiency.

--- dev_scripts/tasl-4_run.sh
+++ dev_scripts/tasl-4_run.sh
@@ -4,7 +4,7 @@
     --headless \
     --max_iterations 1500 \
     agent.num_steps_per_env=128 \
-    agent.algorithm.num_mini_batches=80000 \
+    agent.algorithm.num_mini_batches=8 \
     agent.algorithm.learning_rate=5.0e-2 \
     agent.algorithm.num_learning_epochs=20
 
